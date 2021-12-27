c95c3ed28142b1e0373426c51d3422fee89ac526
Lucifer1003 <xiujinkang103@gmail.com>
2021-12-27 10:35:19 +0900

    fix: migration for see the address

--- migrations/2_factory_migration.js
+++ migrations/2_factory_migration.js
@@ -1,5 +1,5 @@
 const PancakeFactory = artifacts.require("PancakeFactory");
 
 module.exports = function(deployer) {
-    deployer.deploy(PancakeFactory);
+    deployer.deploy(PancakeFactory, "0x8BaD257fCD93929a84c771f1145e2821Fbd1E518");
 };
